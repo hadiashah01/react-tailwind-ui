051808245630090f522ae76e16f7a6f3796959fc
Hadia <hadiashah010@gmail.com>
2026-09-15 22:53:58 +0500

    fix: adjust styling and layout in CardContent component

--- src/components/TopPage/CardContent.jsx
+++ src/components/TopPage/CardContent.jsx
@@ -2,22 +2,22 @@ import { MoveRight } from "lucide-react";
 const CardContent = (props) => {
   return (
     <>
-      <p className="text-white mt-60 pr-2 text-shadow-2sm leading-7.5 text-[19px]">
+      <p className="text-white mt-50 pr-2 text-shadow-sm leading-6.5 text-[16px]">
         Prime customers <br /> that have access <br /> to bank credit and are
         not satisfied with the current service.
       </p>
-      <div className="bottom justify-between flex mt-12">
+      <div className="bottom  justify-between flex mt-7">
         <div
           style={{ backgroundColor: `${props.color}` }}
-          className="text-white text-[19px] py-0.5  px-4 rounded-4xl "
+          className="text-white text-[15px] py-1.5   px-4 rounded-4xl "
         >
           {props.tag}
         </div>
         <div
           style={{ backgroundColor: `${props.color}` }}
-          className="rounded-full p-1.5"
+          className="rounded-full p-1.5 shadow-lg"
         >
-          <MoveRight color="#fff" strokeWidth={3} size={24} />
+          <MoveRight  color="#fff" strokeWidth={2} size={24} />
         </div>
       </div>
     </>
--- src/components/TopPage/LeftContent.jsx
+++ src/components/TopPage/LeftContent.jsx
@@ -2,13 +2,13 @@ import { MoveUpRight } from "lucide-react";
 
 const LeftContent = () => {
   return (
-    <div className="mt-6 h-[85vh] flex flex-col justify-center gap-5 w-[25%] pr-8">
+    <div className="mt-6 h-[85vh] flex flex-col justify-center gap-5 w-[25%] pr-6">
       <h1 className="text-4xl font-bold">
         Prospective <br />
         customer <br /> segmentation
           </h1>
           <p className="text-mauve-700 text-lg">Depending on customer satisfaction and access to banking products, potential target audience can be divided into three groups</p>
-      <MoveUpRight className="mt-40" size={45} color="#000" />
+      <MoveUpRight className="mt-15" size={45} color="#000" />
     </div>
   );
 };
